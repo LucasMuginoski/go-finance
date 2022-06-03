5b8c024134913b4d1bf22879a97319e7f4441ad2
LucasMuginoski <lucasmmuginoski@gmail.com>
2022-06-02 21:36:02 -0300

    style: poppins font implementation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 //importar o themeprovider que é um contexto e disponbilida o theme para import e uso
 import { ThemeProvider } from 'styled-components';
 import  theme  from './src/global/styles/theme';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 
 //importa por padrão o index.tsx 
 import { Dashboard } from './src/screens/dashboard';
@@ -9,6 +17,15 @@ import { Dashboard } from './src/screens/dashboard';
 
 
 export default function App() {
+  //carregando as fontes
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
