444b328715ca2bf419371013e63a367f6a6ecd23
LucasMuginoski <lucasmmuginoski@gmail.com>
2022-06-04 13:46:27 -0300

    feat: header + user image + user greetings text

--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from "react";
-import { Container, Title } from "./styles"; 
+import {Text} from "react-native"
+import { 
+    Container,  
+    Header,
+    UserWraper,
+    UserInfo,
+    Photo,
+    User,
+    UserGrettings,
+    UserName
+} from "./styles"; 
 
 export function Dashboard(){
     return(
-        <Container>
-            <Title>Dashboard!</Title>
+        <Container>      
+            <Header>
+                <UserWraper>
+                    <UserInfo>
+                        <Photo 
+                            source={{uri: 'https://github.com/LucasMuginoski.png'}}
+                        />
+                        <User/>
+                            <UserGrettings>Olá, </UserGrettings>
+                            <UserName>Lucas!</UserName>
+                    </UserInfo>
+                </UserWraper>
+            </Header>
         </Container>
     );
 }
